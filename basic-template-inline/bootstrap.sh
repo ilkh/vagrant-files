5ca3fe124d766377f95f26d5723ba343ff779695
ilkh <i.xarlamov@gmail.com>
2015-11-21 19:26:12 +0300

    added basic template for vagrant with inline provision

---
+++ basic-template-inline/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#https://docs.vagrantup.com/v2/getting-started/provisioning.html
+#https://docs.vagrantup.com/v2/provisioning/shell.html
+
+#sudo dnf upgrade -y
+#if ! [ -L /var/www ]; then
+#  rm -rf /var/www
+#  ln -fs /vagrant /var/www
+#fi
